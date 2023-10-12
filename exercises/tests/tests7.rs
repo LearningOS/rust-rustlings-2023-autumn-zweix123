100ef6c3de63a719f9a3103cf59e6f918097b930
zweix123 <1979803044@qq.com>
2023-10-12 16:02:19 +0800

    debug

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -1,37 +1,10 @@
-// tests7.rs
+// tests8.rs
 //
-// When building packages, some dependencies can neither be imported in
-// `Cargo.toml` nor be directly linked; some preprocesses varies from code
-// generation to set-up package-specific configurations.
+// This execrise shares `build.rs` with the previous exercise.
+// You need to add some code to `build.rs` to make both this exercise and
+// the previous one work.
 //
-// Cargo does not aim to replace other build tools, but it does integrate
-// with them with custom build scripts called `build.rs`. This file is
-// usually placed in the root of the project, while in this case the same
-// directory of this exercise.
-//
-// It can be used to:
-//
-// - Building a bundled C library.
-// - Finding a C library on the host system.
-// - Generating a Rust module from a specification.
-// - Performing any platform-specific configuration needed for the crate.
-//
-// When setting up configurations, we can `println!` in the build script
-// to tell Cargo to follow some instructions. The generic format is:
-//
-//     println!("cargo:{}", your_command_in_string);
-//
-// Please see the official Cargo book about build scripts for more
-// information:
-// https://doc.rust-lang.org/cargo/reference/build-scripts.html
-//
-// In this exercise, we look for an environment variable and expect it to
-// fall in a range. You can look into the testcase to find out the details.
-//
-// You should NOT modify this file. Modify `build.rs` in the same directory
-// to pass this exercise.
-//
-// Execute `rustlings hint tests7` or use the `hint` watch subcommand for a
+// Execute `rustlings hint tests8` or use the `hint` watch subcommand for a
 // hint.
 
 fn main() {}
@@ -42,12 +15,9 @@ mod tests {
 
     #[test]
     fn test_success() {
-        let timestamp = std::time::SystemTime::now()
-            .duration_since(std::time::UNIX_EPOCH)
-            .unwrap()
-            .as_secs();
-        let s = std::env::var("TEST_FOO").unwrap();
-        let e: u64 = s.parse().unwrap();
-        assert!(timestamp >= e && timestamp < e + 10);
+        #[cfg(feature = "pass")]
+        return;
+
+        panic!("no cfg set");
     }
 }
